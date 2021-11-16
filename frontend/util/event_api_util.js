ad765339f151573625c310551b0d403da9e6b0ff
Snigdha  Banda <snigdhabanda@Snigdhas-MacBook-Air.local>
2021-11-16 17:38:28 -0500

    created axios routes

---
+++ frontend/util/event_api_util.js
@@ -0,0 +1,17 @@
+import axios from "axios";
+
+export const createEvent = (event) => {
+    return axios.post("/api/events/newEvent", event)
+}
+
+export const updateEvent = (event) => {
+    return axios.patch(`/api/events/${event.id}`, event)
+}
+
+export const deleteEvent = (eventId) => {
+    return axios.delete(`/api/events/${eventId}`)
+}
+
+export const showEvent = (eventId) => {
+    return axios.get(`/api/events/${eventId}`)
+}
